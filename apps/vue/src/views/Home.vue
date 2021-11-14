009f290e9a37c935b464de3328c0ed7100ddec57
Dmitriy Osipov <dm.osipov@corp.mail.ru>
2021-11-14 23:31:40 +0300

    initial

---
+++ apps/vue/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <hello-world />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "../components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    HelloWorld
+    HelloWorld,
+  },
+});
+</script>
